cbede4bb9cf1adb34a3174f4151cb39e1c669cca
r.osipin <r.osipin@whyhappen.ru>
2025-11-25 00:03:54 +0300

    feat: add test mcp

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Fredoqw.Alfa.ProTerminal.Mcp.Infrastructure.Extensions;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Logging.AddConsole(options => options.LogToStandardErrorThreshold = LogLevel.Trace);
 builder.Services
+    .Register()
     .AddMcpServer()
     .WithStdioServerTransport()
     .WithToolsFromAssembly();
