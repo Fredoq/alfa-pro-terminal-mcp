8e66d93d66bc8a234d19d21a0dad6cbc0a597576
r.osipin <r.osipin@whyhappen.ru>
2025-12-28 20:56:03 +0300

    feat: dispose

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -5,24 +5,23 @@ using Microsoft.Extensions.Logging;
 
 AppSignal signal = new();
 AppServerName name = new();
+using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole(options => options.LogToStandardErrorThreshold = LogLevel.Trace));
 await using AlfaProTerminal terminal = new(new Config
                                             (new EnvironmentVariablesPart
                                                     (new JsonFilesPart
                                                         (new BasePathPart
                                                             (new ConfigurationBuilder(), new AppBasePath()))))
                                         .Root());
-ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole(options => options.LogToStandardErrorThreshold = LogLevel.Trace));
 await new App
-        (signal, new Scope
-            (factory, new TerminalSession
-                (terminal, new EndpointSession
-                    (new TransportLink(name, factory), new EndpointGate
-                        (new OptionsSet
-                            (new ServerInfo
-                                (name, new ApplicationTitle("Alfa Pro Terminal MCP"), new McpVersion(new ProcessPath())),
-                            new CapabilitiesSet(),
-                            new HooksSet(terminal, factory, new Content())),
-                        factory),
-                    signal),
-                signal)))
+        (signal, new TerminalSession
+            (terminal, new EndpointSession
+                (new TransportLink(name, factory), new EndpointGate
+                    (new OptionsSet
+                        (new ServerInfo
+                            (name, new ApplicationTitle("Alfa Pro Terminal MCP"), new McpVersion(new ProcessPath())),
+                        new CapabilitiesSet(),
+                        new HooksSet(terminal, factory, new Content())),
+                    factory),
+                signal),
+            signal))
         .Run();
